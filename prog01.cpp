f2ef8ab90eabc50893a2fa2aa790c559f46cea82
luizhtf123 <167235096+luizhtf123@users.noreply.github.com>
2024-04-23 13:55:06 -0300

    Update prog01.cpp
    foi colocado o laço for para que calculemos 3 triângulos distintos

--- prog01.cpp
+++ prog01.cpp
@@ -5,11 +5,20 @@ main()
 {
 	float pi = 3.14, raio, area;
 
-	printf("Digite o valor do raio: ");
-	scanf("%f", &raio);
 	
-	if(raio > 0){
-		area = pi * (raio * raio );
-		printf("Essa e a area do circulo:%.2f\n", area);
+		for( int i = 0; i < 3; i++){
+			
+			printf("Digite o valor do raio: ");
+			scanf("%f", &raio);
+					
+			if(raio > 0){
+	
+			area = pi * (raio * raio );
+			printf("Essa e a area do circulo:%.2f\n", area);
+	
+		}	else{
+			
+			printf("Valor invalido!\n");	
+		}
 	}
 }
