a804d4ad9c39a03e102f3493f57aa03a83c8906b
luizhtf123 <167235096+luizhtf123@users.noreply.github.com>
2024-04-23 14:47:44 -0300

    Update prog01.cpp
    foi colocado a função switch para operar um menu reciclável com opções de operações

--- prog01.cpp
+++ prog01.cpp
@@ -1,23 +1,54 @@
-//calcula a área de um circulo
 #include <stdio.h>
 
 main()
 {
-	float pi = 3.14, raio, area;
-
+	int op, aux = 0;
 	
-		do{
-			printf("Digite o valor do raio: ");
-			scanf("%f", &raio);
-					
-			if(raio > 0){ 		//esse if é o que faz com que seja declarado 'valor invalido' dentro do DO 
+	while(aux == 0){
 	
-			area = pi * (raio * raio );
-			printf("Essa e a area do circulo:%.2f\n", area);
+	printf("\nEsse programa calcula áreas de circulos, e faz somas.\n digite: \n 1 - calcular círculos\n 2 - Fazer soma\n 3 - Encerrar o programa\n ");
+	scanf("%d", &op);
 	
-		}	else{
+	switch(op){
+		
+		case 1:{
+		
+			float pi = 3.14, raio, area;
 			
-			printf("Valor invalido!\n");
+			do{
+				printf("Digite o valor do raio (0 para sair): ");
+				scanf("%f", &raio);
+					
+				if(raio > 0){ 		//esse if é o que faz com que seja declarado 'valor invalido' dentro do DO 
+	
+				area = pi * (raio * raio );
+				printf("Essa e a area do circulo:%.2f\n", area);
+	
+				}
+				else{
+			
+				printf("Valor invalido!\n");
+					}
+				}while(raio>0);
+		break;
 		}
-	}while(raio>0);
+		
+		case 2:
+			
+			int x,y,soma;
+			printf("Digite separado por espaco os dois valores que deseja somar(digite 123 para sair):");
+			scanf("%d %d", &x, &y);
+			
+			soma = x + y;
+			printf("%d + %d = %d", x, y, soma);
+		break;
+		
+		case 3:
+			aux = 1;	
+			break;
+		
+		default:
+			break;
+		}
+	}
 }
